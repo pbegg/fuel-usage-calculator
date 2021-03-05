fff54fa759e0e9254df0ea72f8161b53ec4aa05a
pbegg <>
2021-03-05 10:07:02 +1000

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,37 @@ module.exports = function (app) {
 
   let record = {}
 
-  
+  let updateDeltaInterval
+
+  let saveFuelUsageInterval
+
+
+  let updateDelta = function(record) {
+      for (const [key, value] of Object.entries(record)) {
+        
+
+        timeTrigger=Date.now()-record[key].fuelUsedTime
+        if (timeTrigger<=5000) {
+          usedPath=key.split('.')
+          usedPath[3]='used'
+          usedPath=usedPath.join('.')
+          app.handleMessage(plugin.id, {
+            updates: [
+              {
+                values: [
+                  {
+                    path: usedPath,
+                    value: value.fuelUsed
+                  }
+                ]
+              }
+            ]
+          })             
+        }
+      }    
+
+  }
+
   let _localSubscription = function(options) {
     const subscribeArray = []
     options.paths.forEach(path => {
@@ -45,6 +75,9 @@ module.exports = function (app) {
         if (options.savedUsage[path] === undefined) {
           options.savedUsage[path] = 0
         }
+        else {
+          record[path]={'fuelUsed':options.savedUsage[path]}
+        }
       })      
     }
     else {
@@ -53,6 +86,7 @@ module.exports = function (app) {
         options.savedUsage[path] = 0
       })
     }
+    _saveFuelUsage(options)
     return options
   }
 
@@ -64,10 +98,10 @@ module.exports = function (app) {
   }
 
   let fuelCalc = function (options,record,path,value,timestamp) {
-    app.debug(record,path,value,timestamp)
-    if (record[path] == undefined) {
+    app.debug(`record is ${JSON.stringify(record)}`)
+    if (record[path]['firstTime'] == undefined) {
       app.debug(`${record[path]} is undefined so creating the first timestamp'`)
-      record[path]={'firstTime':timestamp}
+      record[path]['firstTime']={'firstTime':timestamp}
 
     }
     if (record[path].firstTime) {
@@ -90,6 +124,9 @@ module.exports = function (app) {
               record[path].fuelUsed+=instantFuelUsage
               record[path].fuelUsedTime=Date.now()
             }
+            options.savedUsage[path]=record[path].fuelUsed
+            app.debug(JSON.stringify(options))
+            //_saveFuelUsage(options)
         }      
       record[path].firstTime=timestamp        
       }
@@ -103,6 +140,14 @@ module.exports = function (app) {
     fuelUsage = _loadFuelUsage(options).savedUsage
     app.debug(fuelUsage)
 
+    updateDeltaInterval = setInterval(function() {
+        updateDelta(record);
+    }, 1000);    
+
+    saveFuelUsageInterval = setInterval(function() {
+        _saveFuelUsage(options);
+    }, options.saveFreq);  
+
     app.subscriptionmanager.subscribe(
       _localSubscription(options),
       unsubscribes,
@@ -111,7 +156,7 @@ module.exports = function (app) {
       },
       delta => {
         delta.updates.forEach(u => {
-          fuelCalc(options,record,u.values[0].path,u.values[0].value,Date.parse(u.timestamp))
+          fuelCalc(options,record,_convertRateToUsed([u.values[0].path]),u.values[0].value,Date.parse(u.timestamp))
           app.debug(u);
         });
       }
@@ -123,6 +168,18 @@ module.exports = function (app) {
     app.debug(`${plugin.name} Stopped...`)
     unsubscribes.forEach(f => f());
     unsubscribes = [];
+
+    if (updateDeltaInterval) {
+        clearInterval(updateDeltaInterval);
+    }
+    // clean up the state
+    updateDeltaInterval = undefined;
+
+    if (saveFuelUsageInterval) {
+        clearInterval(saveFuelUsageInterval);
+    }
+    // clean up the state
+    saveFuelUsageInterval = undefined;
   }
 
   const plugin = {
@@ -137,7 +194,7 @@ module.exports = function (app) {
         paths: {
           type: 'array',
           title: 'Paths to use for fuel calculations',
-          default: ['propulsion.port.fuel.rate','propulsion.port.fuel.rate'],
+          default: ['propulsion.port.fuel.rate','propulsion.starboard.fuel.rate'],
           items: {
             type: 'string'
           }
